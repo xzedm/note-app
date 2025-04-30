9055bf63df8dd3b7e5d3771efa7d7e4fd3fcbf14
xzedm <zhuke04@mail.ru>
2025-04-30 15:31:25 +0500

    added moving card functionality

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,10 +1,14 @@
-import {useRef, useEffect} from 'react'
+import {useRef, useEffect, useState} from 'react'
 import Trash from '../icons/Trash';
 
 const NodeCars = ({note}) => {
     const body = JSON.parse(note.body);
-    const position = JSON.parse(note.position);
+    const [position, setPosition] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
+
+    let mouseStartPos = { x: 0, y: 0 };
+    const cardRef = useRef(null);
+    
 
     const textAreaRef = useRef(null);
 
@@ -19,15 +23,53 @@ const NodeCars = ({note}) => {
       current.style.height = current.scrollHeight + "px"; // Set the new height
   }
 
+    const mouseDown = (e) => {
+      mouseStartPos.x = e.clientX;
+      mouseStartPos.y = e.clientY;
+
+      document.addEventListener('mousemove', mouseMove);
+      document.addEventListener('mouseup', mouseUp);
+    }
+
+    const mouseMove = (e) => {
+      let mouseMoveDir = {
+        x: mouseStartPos.x - e.clientX,
+        y: mouseStartPos.y - e.clientY,
+      };
+
+      console.log("mouseMovedir", mouseMoveDir);
+
+      mouseStartPos.x = e.clientX;
+      mouseStartPos.y = e.clientY;
+
+      setPosition({
+        x:cardRef.current.offsetLeft - mouseMoveDir.x,
+        y:cardRef.current.offsetTop - mouseMoveDir.y,
+      });
+    }
+
+    const mouseUp = (e) => {
+      document.removeEventListener('mousemove', mouseMove);
+      document.removeEventListener('mouseup', mouseUp);
+    }
+
+
     return (
-    <div className='card' style={{
+    <div 
+    ref={cardRef} 
+    className='card' 
+    style={{
       backgroundColor: colors.colorBody,
       left: `${position.x}px`,
       top: `${position.y}px`,
       }}>
 
-      <div className='card-header' style={{backgroundColor: colors.colorHeader}}></div>
+      <div
+      onMouseDown={ mouseDown }
+      className='card-header' 
+      style={{backgroundColor: colors.colorHeader}}>
       <Trash />
+      </div>
 
         <div className='card-body'>
           <textarea 
